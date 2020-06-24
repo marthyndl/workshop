44821278721a0c1d130db5f890d01d824512d46b
mdlopez <mdlopez@lanacion.com.ar>
2020-06-23 21:50:51 -0300

    Updates

---
+++ precache-manifest.72ab7cf1791cdf17bb4ec97dad4bf40e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3c92ea518f7b671d11d77a04bb2b6e",
+    "url": "/marthyndl/workshop.git/index.html"
+  },
+  {
+    "revision": "7d955f7732b0a8ec40f6",
+    "url": "/marthyndl/workshop.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1bc9d9a23471ee2961f",
+    "url": "/marthyndl/workshop.git/static/js/2.653b8a97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marthyndl/workshop.git/static/js/2.653b8a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d955f7732b0a8ec40f6",
+    "url": "/marthyndl/workshop.git/static/js/main.48bce41c.chunk.js"
+  },
+  {
+    "revision": "712983ef4c2f052ffef0",
+    "url": "/marthyndl/workshop.git/static/js/runtime-main.674ca0b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marthyndl/workshop.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
